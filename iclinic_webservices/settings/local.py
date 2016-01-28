6849c794519657b05a982dfd5370c79667711fd2
Matheus Rosa <matheusdsrosa@gmail.com>
2016-01-28 00:44:02 -0200

    Add ZipCode model

--- iclinic_webservices/settings/local.py
+++ iclinic_webservices/settings/local.py
@@ -1,1 +1,4 @@
 from iclinic_webservices.settings.base import *
+
+
+INSTALLED_APPS += ('iclinic_webservices.webservices.zipcodes', )
--- iclinic_webservices/webservices/zipcodes/models.py
+++ iclinic_webservices/webservices/zipcodes/models.py
@@ -2,4 +2,29 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Base(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class ZipCode(Base):
+
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=2)
+    address = models.CharField(max_length=255)
+    zip_code = models.CharField(max_length=8)
+    neighborhood = models.CharField(max_length=255)
+
+
+    class Meta:
+        db_table = 'zip_codes'
+        ordering = ['-created_at']
+
+
+    def __unicode__(self):
+        return self.zip_code
+
