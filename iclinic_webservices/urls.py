c5931f789a9f5c8b829ac92d98579248616c81df
Matheus Rosa <matheusdsrosa@gmail.com>
2016-01-28 20:27:08 -0200

    Update urls.py to include all ZipCodeResource urls

--- iclinic_webservices/urls.py
+++ iclinic_webservices/urls.py
@@ -3,5 +3,5 @@ from iclinic_webservices.webservices.zipcodes.api import ZipCodeResource
 
 
 urlpatterns = [
-    url(r'^zipcodes/', ZipCodeResource.as_list())
+    url(r'zipcodes/', include(ZipCodeResource.urls()))
 ]
