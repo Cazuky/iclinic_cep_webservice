e7b13720e97fbc6911244601e0235e153bafdc9d
Matheus Rosa <matheusdsrosa@gmail.com>
2016-01-28 20:27:43 -0200

    Remove Base model

--- iclinic_webservices/webservices/zipcodes/models.py
+++ iclinic_webservices/webservices/zipcodes/models.py
@@ -3,15 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-class Base(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        abstract = True
-
-
-class ZipCode(Base):
+class ZipCode(models.Model):
 
     city = models.CharField(max_length=255)
     state = models.CharField(max_length=2)
@@ -19,11 +11,12 @@ class ZipCode(Base):
     zip_code = models.CharField(max_length=8, unique=True)
     neighborhood = models.CharField(max_length=255)
 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'zip_codes'
         ordering = ['-created_at']
-
 
     def __unicode__(self):
         return self.zip_code
