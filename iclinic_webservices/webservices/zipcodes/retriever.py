bc5528f8d8bb6c072ce2d5afdcea5ebe7715b2e8
Matheus Rosa <matheusdsrosa@gmail.com>
2016-01-28 20:28:28 -0200

    Add docstring for ZipCodeRetriever methods

--- iclinic_webservices/webservices/zipcodes/retriever.py
+++ iclinic_webservices/webservices/zipcodes/retriever.py
@@ -8,6 +8,9 @@ import re
 
 
 class ZipCodeRetriever(object):
+    """
+    This class has the responsability to talk to the Postmon API
+    """
 
     def __init__(self, zip_code):
         if self.validate_zip_code_format(zip_code):
@@ -16,7 +19,23 @@ class ZipCodeRetriever(object):
             raise InvalidZipCodeFormatException
 
     def fetch(self):
-        url = settings.POSTMON_API_URL % { 'cep': self.zip_code }
+        """
+        This method run a GET http request to the Postmon API
+        and returns the information about the zip_code, if exists.
+
+        Result Example:
+         {u'bairro': u'Jardim Am\xe9rica',
+              u'cep': u'14020260',
+              u'cidade': u'Ribeir\xe3o Preto',
+              u'cidade_info': {u'area_km2': u'650,955', u'codigo_ibge': u'3543402'},
+              u'complemento': u'at\xe9 489 - lado \xedmpar',
+              u'estado': u'SP',
+              u'estado_info': {u'area_km2': u'248.222,362',
+               u'codigo_ibge': u'35',
+               u'nome': u'S\xe3o Paulo'},
+          u'logradouro': u'Avenida Presidente Vargas'})
+        """
+        url = settings.POSTMON_API_URL % {'cep': self.zip_code}
         response = requests.get(url)
 
         if response.status_code == 404:
