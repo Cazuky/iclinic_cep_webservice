94e60a078d3e5feae6b453f98e389ba4825f8b45
Matheus Rosa <matheusdsrosa@gmail.com>
2016-01-29 02:01:26 -0200

    Add manage.py command to create api key easily

---
+++ iclinic_webservices/webservices/apikeys/management/commands/createapikey.py
@@ -0,0 +1,22 @@
+import hashlib
+import random
+import string
+
+from django.core.management.base import BaseCommand
+
+from iclinic_webservices.webservices.apikeys.models import ApiKey
+
+
+class Command(BaseCommand):
+    help = "Creates a API Key"
+
+    def random_string(self, length=10):
+        return ''.join(random.choice(string.ascii_letters) for x in xrange(length))
+
+    def handle(self, *args, **options):
+        md5 = hashlib.md5()
+        md5.update(self.random_string())
+
+        api_key = ApiKey.objects.create(key=md5.hexdigest(), active=True)
+
+        self.stdout.write(self.style.SUCCESS("API Key created: %s" % api_key.key))
