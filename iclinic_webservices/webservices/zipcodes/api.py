43dd0748b6cb1da424130947b0ed035c6942bb36
Matheus Rosa <matheusdsrosa@gmail.com>
2016-01-30 17:04:19 -0200

    Add `limit` support for `list` method and refactor `create` method

--- iclinic_webservices/webservices/zipcodes/api.py
+++ iclinic_webservices/webservices/zipcodes/api.py
@@ -1,3 +1,7 @@
+"""
+    api.py
+    This is the module that handle the API calls
+"""
 import logging
 
 from restless.dj import DjangoResource
@@ -11,13 +15,17 @@ from iclinic_webservices.webservices.zipcodes.models import ZipCode
 from iclinic_webservices.webservices.apikeys.models import ApiKey
 from iclinic_webservices.webservices.zipcodes.retriever import ZipCodeRetriever
 
-from iclinic_webservices.webservices.zipcodes.exceptions import InvalidZipCodeFormatException, PostmonZipCodeNotFound
+from iclinic_webservices.webservices.zipcodes.exceptions import \
+    InvalidZipCodeFormatException, PostmonZipCodeNotFound
 
 
 logger = logging.getLogger(__name__)
 
 
 class ZipCodeResource(DjangoResource):
+    """
+        This is the ZipCode API
+    """
     preparer = FieldsPreparer(fields={
         'id': 'id',
         'city': 'city',
@@ -33,77 +41,84 @@ class ZipCodeResource(DjangoResource):
     def is_authenticated(self):
         try:
             api_key = ApiKey.objects.get(key=self.request.GET.get('api_key'))
-            if api_key.active:
-                return True
-            else:
-                return False
+            return api_key.active
         except ApiKey.DoesNotExist:
             return False
 
     def list(self):
-        logger.info('[API LIST] List zipcodes')
-        return ZipCode.objects.all()
+        limit = self.request.GET.get('limit')
+        logger.info('[API LIST] List zipcodes limit=%s', limit)
+        zip_codes = ZipCode.objects.all()
+
+        if limit:
+            return zip_codes[:limit]
+
+        return zip_codes
 
     def create(self):
-        logger.info('[API CREATE] Create zipcode. zip_code=%s' % self.data.get('zip_code'))
         zip_code = self.data.get('zip_code')
+
+        logger.info('[API CREATE] Create zipcode. zip_code=%s', zip_code)
 
         try:
             retriever = ZipCodeRetriever(zip_code)
-            data = retriever.fetch()
-            logger.info('[API CREATE] Create zipcode. zip_code=%s data=%s' % (zip_code, data))
         except InvalidZipCodeFormatException:
             error = "Invalid %s zip code format." % zip_code
-            logger.error('[API CREATE] Error on create zipcode. zip_code=%s error=%s' % (zip_code, error))
+            logger.error('[API CREATE] Error on create zipcode. '
+                         'zip_code=%s error=%s', zip_code, error)
 
             raise BadRequest(error)
-
-        except PostmonZipCodeNotFound:
-            error = "Zipcode %s not found in Postmon." % zip_code
-            logger.error('[API CREATE] Error on create zipcode. zip_code=%s error=%s' % (zip_code, error))
-
-            raise BadRequest(error)
-
-        zip_code_data = {
-            'city': data.get('cidade'),
-            'state': data.get('estado'),
-            'zip_code': data.get('cep'),
-            'address': data.get('logradouro'),
-            'neighborhood': data.get('bairro')
-        }
 
         try:
-            zip_code_object = ZipCode.objects.create(**zip_code_data)
+            data = retriever.fetch()
+            logger.info('[API CREATE] Create zipcode. zip_code=%s data=%s',
+                        zip_code, data)
 
-            logger.info('[API CREATE] Zipcode created. zip_code=%s' % zip_code)
+            zip_code_data = {
+                'city': data.get('cidade'),
+                'state': data.get('estado'),
+                'zip_code': data.get('cep'),
+                'address': data.get('logradouro'),
+                'neighborhood': data.get('bairro')
+            }
 
-            return zip_code_object
-        except IntegrityError as e:
-            error_code, error_message = e
-            logger.error('[API CREATE] Error on create zipcode. zip_code=%s error=%s' % (zip_code, error_message))
+            try:
+                zip_code_object = ZipCode.objects.create(**zip_code_data)
+                logger.info('[API CREATE] Zipcode created. zip_code=%s', zip_code)
+                return zip_code_object
+            except IntegrityError as exception:
+                error_message = exception[1]
+                logger.error('[API CREATE] Error on create zipcode. '
+                             'zip_code=%s error=%s', zip_code, error_message)
 
-            raise BadRequest(error_message)
+                raise BadRequest(error_message)
+        except PostmonZipCodeNotFound:
+            error = "Zipcode %s not found in Postmon." % zip_code
+            logger.error('[API CREATE] Error on create zipcode. '
+                         'zip_code=%s error=%s', zip_code, error)
+
+            raise BadRequest(error)
 
     def detail(self, zip_code):
-        logger.info('[API DETAIL] Get zipcode details. zip_code=%s' % zip_code)
+        logger.info('[API DETAIL] Get zipcode details. zip_code=%s', zip_code)
         try:
             zip_code_object = ZipCode.objects.get(zip_code=zip_code)
         except ZipCode.DoesNotExist:
             error = "Zipcode %s not found in the database." % zip_code
-            logger.info("[API DETAIL] Error on getting zipcode details. zip_code=%s error=%s" % (zip_code, error))
-
+            logger.info('[API DETAIL] Error on getting zipcode details. '
+                        'zip_code=%s error=%s', zip_code, error)
             raise NotFound(error)
 
         return zip_code_object
 
     def delete(self, zip_code):
-        logger.info('[API DELETE] Delete zipcode. zip_code=%s' % zip_code)
+        logger.info('[API DELETE] Delete zipcode. zip_code=%s', zip_code)
         try:
             zip_code_object = ZipCode.objects.get(zip_code=zip_code)
         except ZipCode.DoesNotExist:
             error = "Zipcode %s not found in the database." % zip_code
-            logger.info("[API DELETE] Error on deleting zipcode details. zip_code=%s error=%s" % (zip_code, error))
-
+            logger.info('[API DELETE] Error on deleting zipcode details. '
+                        'zip_code=%s error=%s', zip_code, error)
             raise NotFound(error)
 
         zip_code_object.delete()
--- iclinic_webservices/webservices/zipcodes/tests/zipcode_resource_test.py
+++ iclinic_webservices/webservices/zipcodes/tests/zipcode_resource_test.py
@@ -1,21 +1,31 @@
+"""
+    zipcode_resource_test.py
+    Test file for the ZipCodeResource class
+"""
+import json
+
 from django.test import TestCase, Client
 
 from .factories import ZipCodeFactory, ApiKeyFactory
 from iclinic_webservices.webservices.zipcodes.models import ZipCode
 
-import json
-
 
 class ZipCodeResourceTestCase(TestCase):
-
+    """
+        TestCase for the ZipCode API
+    """
     def setUp(self):
         self.client = Client()
         self.api_key = ApiKeyFactory.create()
         self.url = "/zipcodes/?api_key=%s" % self.api_key
+        self.url_with_limit = "/zipcodes/?limit=%(limit)s&api_key=%(api_key)s"
         self.url_with_zipcode = "/zipcodes/%(zip_code)s/?api_key=%(api_key)s"
         self.fake_zipcode = '000-12345'
 
     def test_list(self):
+        """
+            Tests for the /zipcodes/ endpoint
+        """
         zip_code = ZipCodeFactory.create()
         response = self.client.get(self.url)
         expected = '{"objects": [%s]}' % zip_code.jsonify()
@@ -23,7 +33,27 @@ class ZipCodeResourceTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content, expected)
 
+    def test_list_with_limit(self):
+        """
+            Tests for the /zipcodes/?limit=XXX endpoint
+        """
+        zip_codes = [
+            ZipCodeFactory.create(),
+            ZipCodeFactory.create(),
+            ZipCodeFactory.create()
+        ]
+
+        url = self.url_with_limit % {'limit': 1, 'api_key': self.api_key}
+        response = self.client.get(url)
+        expected = '{"objects": [%s]}' % zip_codes[2].jsonify()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, expected)
+
     def test_create(self):
+        """
+            Tests for the /zipcodes/ endpoint with POST method
+        """
         zip_code_json = json.dumps({"zip_code": "14020260"})
         response = self.client.post(self.url,
                                     zip_code_json,
@@ -37,6 +67,9 @@ class ZipCodeResourceTestCase(TestCase):
         self.assertJSONEqual(response.content, expected)
 
     def teste_delete(self):
+        """
+            Tests for the /zipcodes/<zip_code> endpoint with DELETE method
+        """
         zip_code = ZipCodeFactory.create()
         url = self.url_with_zipcode % {'zip_code': zip_code.zip_code,
                                        'api_key': self.api_key}
@@ -46,6 +79,9 @@ class ZipCodeResourceTestCase(TestCase):
         self.assertEqual(response.status_code, 204)
 
     def test_detail(self):
+        """
+            Tests for the /zipcodes/<zip_code> endpoint
+        """
         zip_code = ZipCodeFactory.create()
         url = self.url_with_zipcode % {'zip_code': zip_code.zip_code,
                                        'api_key': self.api_key}
@@ -55,6 +91,10 @@ class ZipCodeResourceTestCase(TestCase):
         self.assertJSONEqual(response.content, zip_code.jsonify())
 
     def test_create_zipcode_duplicated(self):
+        """
+            Tests for the /zipcodes/ endpoint with POST method trying to insert
+            the same zipcode into the database.
+        """
         zip_code_json = json.dumps({"zip_code": "14800360"})
 
         # create zipcode successfullly
@@ -70,6 +110,9 @@ class ZipCodeResourceTestCase(TestCase):
         self.assertEqual(response.status_code, 400)
 
     def test_detail_not_found(self):
+        """
+            Tests for the /zipcodes/<zip_code> when zip_code does not exist
+        """
         zip_code = '00000000'
         url = self.url_with_zipcode % {'zip_code': zip_code,
                                        'api_key': self.api_key}
@@ -78,6 +121,10 @@ class ZipCodeResourceTestCase(TestCase):
         self.assertEqual(response.status_code, 404)
 
     def test_delete_not_found(self):
+        """
+            Tests for the /zipcodes/<zip_code> with DELETE method
+            when zip_code does not exist
+        """
         zip_code = '00000000'
         url = self.url_with_zipcode % {'zip_code': zip_code,
                                        'api_key': self.api_key}
@@ -85,7 +132,11 @@ class ZipCodeResourceTestCase(TestCase):
 
         self.assertEqual(response.status_code, 404)
 
-    def test_create_zipcode_invalid_format(self):
+    def test_create_invalid_format(self):
+        """
+            Tests for the /zipcodes/ with the POST method when zip_code
+            doesn't have a valid format
+        """
         zip_code_json = json.dumps({'zip_code': self.fake_zipcode})
         error_message = "Invalid %s zip code format." % self.fake_zipcode
 
@@ -95,7 +146,11 @@ class ZipCodeResourceTestCase(TestCase):
         self.assertEqual(response.status_code, 400)
         self.assertIn(error_message, response.content)
 
-    def test_create_zipcode_not_found_in_postmon(self):
+    def test_create_not_found_postmon(self):
+        """
+            Tests for the /zipcodes/ with the POST method when Postmon
+            doesn't find the zip_code
+        """
         zip_code = '00000-123'
         zip_code_json = json.dumps({'zip_code': zip_code})
         error_message = "Zipcode %s not found in Postmon." % zip_code
