70dbc43a081bbc8df3d031f4aa204c6dcb50243e
Matheus Rosa <matheusdsrosa@gmail.com>
2016-01-30 17:06:08 -0200

    Extract `validate_zip_code_format` method to a function and organize imports in ZipCodeRetriever

--- iclinic_webservices/webservices/zipcodes/retriever.py
+++ iclinic_webservices/webservices/zipcodes/retriever.py
@@ -1,14 +1,31 @@
-from .models import ZipCode
-from .exceptions import InvalidZipCodeFormatException, PostmonZipCodeNotFound
-from django.conf import settings
+"""
+  retriever.py
+  This is module contains the ZipCodeRetriever class that has the
+  responsability to fetch information about the zipcode using the
+  Postmon API
+"""
 
-import requests
 import logging
 import json
 import re
 
+import requests
+
+from .exceptions import InvalidZipCodeFormatException, PostmonZipCodeNotFound
+from django.conf import settings
+
 
 logger = logging.getLogger(__name__)
+
+
+def validate_zip_code_format(zip_code):
+    """
+      Validate a zip_code format
+    """
+    match = re.match(r'^\d{5}\-{0,1}\d{3}$', zip_code)
+    if match:
+        return True
+    return False
 
 
 class ZipCodeRetriever(object):
@@ -17,7 +34,7 @@ class ZipCodeRetriever(object):
     """
 
     def __init__(self, zip_code):
-        if self.validate_zip_code_format(zip_code):
+        if validate_zip_code_format(zip_code):
             self.zip_code = zip_code
         else:
             raise InvalidZipCodeFormatException
@@ -31,7 +48,8 @@ class ZipCodeRetriever(object):
          {u'bairro': u'Jardim Am\xe9rica',
               u'cep': u'14020260',
               u'cidade': u'Ribeir\xe3o Preto',
-              u'cidade_info': {u'area_km2': u'650,955', u'codigo_ibge': u'3543402'},
+              u'cidade_info': {u'area_km2': u'650,955',
+                               u'codigo_ibge': u'354
               u'complemento': u'at\xe9 489 - lado \xedmpar',
               u'estado': u'SP',
               u'estado_info': {u'area_km2': u'248.222,362',
@@ -39,24 +57,22 @@ class ZipCodeRetriever(object):
                u'nome': u'S\xe3o Paulo'},
           u'logradouro': u'Avenida Presidente Vargas'})
         """
-        logger.info('[POSTMON] Fecthing zipcode information. zip_code=%s' % self.zip_code)
+        logger.info('[POSTMON] Fecthing zip_code=%s', self.zip_code)
         url = settings.POSTMON_API_URL % {'cep': self.zip_code}
         response = requests.get(url)
 
+        if response.status_code not in [404, 200]:
+            return response.status_code, response.text
+
         if response.status_code == 404:
-            logger.info('[POSTMON] Zipcode not found. zip_code=%s' % self.zip_code)
+            logger.info('[POSTMON] Zipcode not found. zip_code=%s',
+                        self.zip_code)
             raise PostmonZipCodeNotFound
 
         if response.status_code == 200:
-            logger.info('[POSTMON] Zipcode found. zip_code=%s' % self.zip_code)
+            logger.info('[POSTMON] Zipcode found. zip_code=%s', self.zip_code)
             return json.loads(response.text)
 
-        logger.info('[POSTMON] Fetched zipcode information. zip_code=%s status_code=%s response=%s' % (self.zip_code, response.status_code, response.text))
-
-        return response.status_code, response.text
-
-    def validate_zip_code_format(self, zip_code):
-        match = re.match(r'^\d{5}\-{0,1}\d{3}$', zip_code)
-        if match:
-            return True
-        return False
+        logger.info('[POSTMON] Done fetching zipcode. zip_code=%s'
+                    ' status_code=%s response=%s', self.zip_code,
+                    response.status_code, response.text)
